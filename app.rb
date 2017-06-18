06bf00dd3c9acb6e17d4029ac703f7e4e77df06a
Manlibear <will.prosser@gmail.com>
2017-06-18 16:27:44 +0000

    JSON Proto

--- app.rb
+++ app.rb
@@ -1,1 +1,11 @@
-print"It's Assayer, by D0pam1ne and Manlibear!"
+require 'net/http'
+require 'json'
+ 
+url = 'https://ohi-api.code4hr.org/vendors?name=Havana'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)
+
+data.each do |child|
+    puts child[1]['name'] + " scored a " + child[1]['score'].to_s
+end
